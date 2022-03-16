774ee5316f1f3d7a5cdfb05b1573d00e10041bed
Олег Матшин <89175955650@mail.ru>
2022-03-17 00:24:31 +0500

    Final commit HomeTask3

--- HomeTask_3_160322.py
+++ HomeTask_3_160322.py
@@ -13,21 +13,34 @@ def list_files():
             list_txt_files.append(item1)
     return list_txt_files
 
-# reading txt-files function
+# Reading txt-files function
 def read_txt(file):
     with open(file, 'r', encoding='utf-8') as f:
         rt = f.readlines()
     return rt
 
-# create sorted files dictionary
+# Create sorted files dictionary
 def files_dict():
     files_txt_dict = {}
     lf = list_files()
     for item2 in lf:
         files_txt_dict[item2] = len(read_txt(file=item2))
-    list_txt_sort = dict(sorted(files_txt_dict.items(), key=lambda item: item[1]))
-    return list_txt_sort
+    dict_txt_sort = dict(sorted(files_txt_dict.items(), key=lambda item: item[1]))
+    return dict_txt_sort
 
-print(list_files())
-print(read_txt('3.txt.txt'))
-print(files_dict())
+# Create total_file.txt function
+def total_file():
+    with open('total_file.txt', 'w', encoding='utf-8') as f1:
+        for item3, item4 in files_dict().items():
+            if item3 == 'total_file.txt':
+                continue
+            f1.write(f'{item3}\n')
+            f1.write(f'{item4}\n')
+            with open(str(item3), 'r', encoding='utf-8') as f2:
+                f1.write(f'{f2.read()}\n')
+
+# Test results
+# print(list_files())
+# print(read_txt('2.txt.txt'))
+# print(files_dict())
+# total_file()
