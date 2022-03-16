f20f3801e45e7a9506aa33a0e8a7693a75ff238c
Олег Матшин <89175955650@mail.ru>
2022-03-16 19:52:21 +0500

    Modified function with control of doubling

--- HomeTask_2_160322.py
+++ HomeTask_2_160322.py
@@ -32,14 +32,18 @@ def print_book():
         print('\t],')
     print('}\n', '(-_-)'*20)
 
-# get_shop_list function
+# get_shop_list function with doubling control
 def get_shop_list_by_dishes(text_file, dishes, person_count):
     shop_list_by_dishes = {}
     shop_list = {}
     for i in dishes:
         for j, k in cook_book(text_file).items():
-            if i == j:
+            if i == j and j not in shop_list_by_dishes:
                 shop_list_by_dishes[j] = k 
+            elif i == j and j in shop_list_by_dishes:
+                for item1, item2 in shop_list_by_dishes.items():
+                    for item3 in item2:
+                        item3['quantity'] = int(item3['quantity']) * 2
     for l, n in shop_list_by_dishes.items():
         for p in n:
             meas_quant = {}
@@ -48,4 +52,10 @@ def get_shop_list_by_dishes(text_file, dishes, person_count):
             shop_list[p['ingredient_name']] = meas_quant
     return shop_list
 
+# print result
 print(get_shop_list_by_dishes('recipes.txt', ['Запеченный картофель', 'Омлет'], 2))
+print('-----'*20)
+# control print result with doubling
+print(get_shop_list_by_dishes('recipes.txt', ['Омлет', 'Омлет'], 2))
+print('-----'*20)
+print(get_shop_list_by_dishes('recipes.txt', ['Омлет'], 4))
